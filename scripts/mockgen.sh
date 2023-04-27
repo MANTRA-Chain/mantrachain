c94ba02cbd0575dc45d38605655e5a98060bd35d
Vladimir Trifonov <trifonov.vp@gmail.com>
2023-04-27 10:02:08 +0300

    wip: add token module tests

--- scripts/mockgen.sh
+++ scripts/mockgen.sh
@@ -3,3 +3,4 @@
 mockgen_cmd="mockgen"
 $mockgen_cmd -source=x/nft/types/expected_keepers.go -package testutil -destination x/nft/testutil/expected_keepers_mocks.go
 $mockgen_cmd -source=x/guard/types/expected_keepers.go -package testutil -destination x/guard/testutil/expected_keepers_mocks.go
+$mockgen_cmd -source=x/token/types/expected_keepers.go -package testutil -destination x/token/testutil/expected_keepers_mocks.go
