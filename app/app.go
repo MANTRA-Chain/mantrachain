7e8c702652fe2e138472838db04551b6b0702ffb
Mantrachain Support <security@mantrachain.io>
2024-09-09 23:42:51 +0800

    begin "use osmosis token factory"

--- app/app.go
+++ app/app.go
@@ -23,7 +23,6 @@ import (
 	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
 	_ "github.com/MANTRA-Chain/mantrachain/client/docs/statik" // import for side-effects
 	_ "github.com/MANTRA-Chain/mantrachain/x/tokenfactory"     // import for side-effects
-	tokenfactorykeeper "github.com/MANTRA-Chain/mantrachain/x/tokenfactory/keeper"
 	abci "github.com/cometbft/cometbft/abci/types"
 	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
 	dbm "github.com/cosmos/cosmos-db"
@@ -82,6 +81,7 @@ import (
 	ibctransferkeeper "github.com/cosmos/ibc-go/v8/modules/apps/transfer/keeper"
 	ibckeeper "github.com/cosmos/ibc-go/v8/modules/core/keeper"
 	"github.com/gorilla/mux"
+	tokenfactorykeeper "github.com/osmosis-labs/osmosis/v26/x/tokenfactory/keeper"
 	"github.com/rakyll/statik/fs"
 	_ "github.com/skip-mev/connect/v2/x/marketmap" // import for side-effects
 	marketmapkeeper "github.com/skip-mev/connect/v2/x/marketmap/keeper"
