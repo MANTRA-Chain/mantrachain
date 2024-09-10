d5d9aeb12878e2510dd5b3b5464aae4944f7265e
Mantrachain Support <security@mantrachain.io>
2024-09-10 19:50:18 +0800

    adopt a different style for registering interfaces

--- app/tokenfactory.go
+++ app/tokenfactory.go
@@ -3,8 +3,10 @@ package app
 import (
 	"fmt"
 
+	"cosmossdk.io/core/appmodule"
 	storetypes "cosmossdk.io/store/types"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/osmosis-labs/osmosis/v26/x/tokenfactory"
 	tokenfactorykeeper "github.com/osmosis-labs/osmosis/v26/x/tokenfactory/keeper"
 	tokenfactorytypes "github.com/osmosis-labs/osmosis/v26/x/tokenfactory/types"
@@ -40,7 +42,17 @@ func (app *App) registerTokenFactoryModule() error {
 	return nil
 }
 
-// RegisterTokenFactory registers the TokenFactory module's types for the given codec.
-func RegisterTokenFactory(registry codectypes.InterfaceRegistry) {
-	tokenfactorytypes.RegisterInterfaces(registry)
+// RegisterIBC Since the IBC modules don't support dependency injection,
+// we need to manually register the modules on the client side.
+// This needs to be removed after IBC supports App Wiring.
+func RegisterTokenFactory(registry codectypes.InterfaceRegistry) map[string]appmodule.AppModule {
+	modules := map[string]appmodule.AppModule{
+		tokenfactorytypes.ModuleName: tokenfactory.AppModule{},
+	}
+
+	for name, m := range modules {
+		module.CoreAppModuleBasicAdaptor(name, m).RegisterInterfaces(registry)
+	}
+
+	return modules
 }
