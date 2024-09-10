7b93e8d1f0e31bbfa2c7f072e12d36e36bf27df3
Mantrachain Support <security@mantrachain.io>
2024-09-10 23:19:11 +0800

    IBC rate limits

--- app/ibcratelimit.go
+++ app/ibcratelimit.go
@@ -1,1 +1,61 @@
 package app
+
+import (
+	"fmt"
+
+	"cosmossdk.io/core/appmodule"
+	storetypes "cosmossdk.io/store/types"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/osmosis-labs/osmosis/v26/x/tokenfactory"
+	tokenfactorykeeper "github.com/osmosis-labs/osmosis/v26/x/tokenfactory/keeper"
+	tokenfactorytypes "github.com/osmosis-labs/osmosis/v26/x/tokenfactory/types"
+)
+
+func (app *App) registerIBCRateLimit() error {
+	// Set up store keys
+	if err := app.RegisterStores(
+		storetypes.NewKVStoreKey(tokenfactorytypes.StoreKey),
+	); err != nil {
+		return err
+	}
+
+	// Ensure the subspace is properly initialized
+	app.ParamsKeeper.Subspace(tokenfactorytypes.ModuleName)
+
+	// Create TokenFactory Keeper
+	tokenFactoryKeeper := tokenfactorykeeper.NewKeeper(
+		app.GetKey(tokenfactorytypes.StoreKey),
+		app.GetSubspace(tokenfactorytypes.ModuleName),
+		GetMaccPerms(),
+		app.AccountKeeper,
+		app.BankKeeper,
+		app.DistrKeeper,
+	)
+	app.TokenFactoryKeeper = tokenFactoryKeeper
+
+	// Create TokenFactory Module
+	tokenFactoryModule := tokenfactory.NewAppModule(app.TokenFactoryKeeper, app.AccountKeeper, app.BankKeeper)
+
+	// Register the TokenFactory module
+	if err := app.RegisterModules(tokenFactoryModule); err != nil {
+		return fmt.Errorf("failed to register TokenFactory module: %w", err)
+	}
+
+	return nil
+}
+
+// RegisterIBC Since the IBC modules don't support dependency injection,
+// we need to manually register the modules on the client side.
+// This needs to be removed after IBC supports App Wiring.
+func RegisterIBCRateLimit(registry codectypes.InterfaceRegistry) map[string]appmodule.AppModule {
+	modules := map[string]appmodule.AppModule{
+		tokenfactorytypes.ModuleName: tokenfactory.AppModule{},
+	}
+
+	for name, m := range modules {
+		module.CoreAppModuleBasicAdaptor(name, m).RegisterInterfaces(registry)
+	}
+
+	return modules
+}
