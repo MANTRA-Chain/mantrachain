4bc78ca44758a2d6799fa1c05fe194692a8db788
Mantrachain Support <security@mantrachain.io>
2024-09-16 13:57:10 +0800

    remove beforesend whitelist

--- x/tokenfactory/types/params.go
+++ x/tokenfactory/types/params.go
@@ -12,12 +12,10 @@ var (
 	KeyDenomCreationFee        = []byte("DenomCreationFee")
 	KeyDenomCreationGasConsume = []byte("DenomCreationGasConsume")
 	KeyFeeCollectorAddress     = []byte("FeeCollectorAddress")
-	KeyWhitelistedHooks        = []byte("WhitelistedHooks")
 	// We don't want to charge users for denom creation
 	DefaultDenomCreationFee        sdk.Coins
 	DefaultDenomCreationGasConsume uint64
 	DefaultFeeCollectorAddress     = ""
-	DefaultWhitelistedHooks        = []*WhitelistedHook{}
 )
 
 // ParamKeyTable the param key table for tokenfactory module.
@@ -25,18 +23,17 @@ func ParamKeyTable() paramtypes.KeyTable {
 	return paramtypes.NewKeyTable().RegisterParamSet(&Params{})
 }
 
-func NewParams(denomCreationFee sdk.Coins, denomCreationGasConsume uint64, feeCollectorAddress string, whitelistedHooks []*WhitelistedHook) Params {
+func NewParams(denomCreationFee sdk.Coins, denomCreationGasConsume uint64, feeCollectorAddress string) Params {
 	return Params{
 		DenomCreationFee:        denomCreationFee,
 		DenomCreationGasConsume: denomCreationGasConsume,
 		FeeCollectorAddress:     feeCollectorAddress,
-		WhitelistedHooks:        whitelistedHooks,
 	}
 }
 
 // DefaultParams returns a default set of parameters
 func DefaultParams() Params {
-	return NewParams(DefaultDenomCreationFee, DefaultDenomCreationGasConsume, DefaultFeeCollectorAddress, DefaultWhitelistedHooks)
+	return NewParams(DefaultDenomCreationFee, DefaultDenomCreationGasConsume, DefaultFeeCollectorAddress)
 }
 
 // Validate validates params
@@ -57,10 +54,6 @@ func (p Params) Validate() error {
 		return fmt.Errorf("failed to validate FeeCollectorAddress: %w", err)
 	}
 
-	if err := validateWhitelistedHooks(p.WhitelistedHooks); err != nil {
-		return fmt.Errorf("failed to validate WhitelistedHooks: %w", err)
-	}
-
 	return nil
 }
 
@@ -70,7 +63,6 @@ func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
 		paramtypes.NewParamSetPair(KeyDenomCreationFee, &p.DenomCreationFee, validateDenomCreationFee),
 		paramtypes.NewParamSetPair(KeyDenomCreationGasConsume, &p.DenomCreationGasConsume, validateDenomCreationGasConsume),
 		paramtypes.NewParamSetPair(KeyFeeCollectorAddress, &p.FeeCollectorAddress, validateFeeCollectorAddress),
-		paramtypes.NewParamSetPair(KeyWhitelistedHooks, &p.WhitelistedHooks, validateWhitelistedHooks),
 	}
 }
 
@@ -111,26 +103,5 @@ func validateFeeCollectorAddress(i interface{}) error {
 		return fmt.Errorf("invalid fee collector address: %w", err)
 	}
 
-	return nil
-}
-
-func validateWhitelistedHooks(i interface{}) error {
-	hooks, ok := i.([]*WhitelistedHook)
-	if !ok {
-		return fmt.Errorf("invalid parameter type: %T", i)
-	}
-
-	seenHooks := map[string]bool{}
-	for _, hook := range hooks {
-		hookStr := hook.String()
-		if seenHooks[hookStr] {
-			return fmt.Errorf("duplicate whitelisted hook: %s", hookStr)
-		}
-		seenHooks[hookStr] = true
-		_, err := sdk.AccAddressFromBech32(hook.DenomCreator)
-		if err != nil {
-			return fmt.Errorf("invalid denom creator address: %w", err)
-		}
-	}
 	return nil
 }
