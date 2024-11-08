3eaa0f3b73868603e319aaadcedebf35289a46d4
Mantrachain Development Support <security@mantrachain.io>
2024-11-08 13:41:06 +0900

    Revert "fix: register uom as DefaultBondDenom (#195)" (#221)
    This reverts commit 1596ac0b56de7c22c31abd358cfe77627ae43bfa.

--- app/params/config.go
+++ app/params/config.go
@@ -1,17 +1,8 @@
 package params
 
 import (
-	"cosmossdk.io/math"
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-)
-
-const (
-	HumanCoinUnit = "om"
-	BaseCoinUnit  = "uom"
-	OmExponent    = 6
-
-	DefaultBondDenom = BaseCoinUnit
 )
 
 var (
@@ -29,27 +20,7 @@ var (
 )
 
 func init() {
-	sdk.SetCoinDenomRegex(MantraCoinDenomRegex)
 	SetAddressPrefixes()
-	RegisterDenoms()
-}
-
-// MantraCoinDenomRegex returns the mantra regex string
-// this is used to override the default sdk coin denom regex
-func MantraCoinDenomRegex() string {
-	return `[a-zA-Z][a-zA-Z0-9/:._-]{1,127}`
-}
-
-// RegisterDenoms registers token denoms.
-func RegisterDenoms() {
-	err := sdk.RegisterDenom(HumanCoinUnit, math.LegacyOneDec())
-	if err != nil {
-		panic(err)
-	}
-	err = sdk.RegisterDenom(BaseCoinUnit, math.LegacyNewDecWithPrec(1, OmExponent))
-	if err != nil {
-		panic(err)
-	}
 }
 
 // SetAddressPrefixes builds the Config with Bech32 addressPrefix and publKeyPrefix for accounts, validators, and consensus nodes and verifies that addreeses have correct format.
