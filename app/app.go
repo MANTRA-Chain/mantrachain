c511318479e267a9de380dddc23a21eafcbbbac7
freeelancer <lance.lan@mantra.finance>
2025-03-06 16:23:02 +0800

    updated swagger to include evmos modules

--- app/app.go
+++ app/app.go
@@ -1305,7 +1305,6 @@ func RegisterSwaggerAPI(ctx client.Context, rtr *mux.Router) {
 	}
 
 	staticServer := http.FileServer(statikFS)
-	rtr.PathPrefix("/static/").Handler(http.StripPrefix("/static/", staticServer))
 	rtr.PathPrefix("").Handler(staticServer)
 }
 
