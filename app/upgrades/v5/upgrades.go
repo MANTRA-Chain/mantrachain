9414600c050f4b3f215c69703255d4ace788c925
freeelancer <lance.lan@mantra.finance>
2025-05-23 15:23:12 +0800

    upgrade from v4 to v5-evm

--- app/upgrades/v5/upgrades.go
+++ app/upgrades/v5/upgrades.go
@@ -3,10 +3,30 @@ package v5
 import (
 	"context"
 
+	storetypes "cosmossdk.io/store/types"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	"github.com/MANTRA-Chain/mantrachain/v5/app/upgrades"
+	"github.com/cosmos/cosmos-sdk/runtime"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	feemarkettypes "github.com/cosmos/evm/x/feemarket/types"
+)
+
+const (
+	skipFeemarketPrefixParams = iota + 1
+	skipFeemarketPrefixState
+	skipFeemarketPrefixEnableHeight = 3
+)
+
+var (
+	// KeyParams is the store key for the feemarket module's parameters.
+	skipKeyParams = []byte{skipFeemarketPrefixParams}
+
+	// KeyState is the store key for the feemarket module's data.
+	skipKeyState = []byte{skipFeemarketPrefixState}
+
+	// KeyEnabledHeight is the store key for the feemarket module's enabled height.
+	skipKeyEnabledHeight = []byte{skipFeemarketPrefixEnableHeight}
 )
 
 func CreateUpgradeHandler(
@@ -17,6 +37,21 @@ func CreateUpgradeHandler(
 	return func(c context.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		ctx := sdk.UnwrapSDKContext(c)
 		ctx.Logger().Info("Starting module migrations...")
+
+		// delete skip-mev feemarket
+		feemarketstore := runtime.NewKVStoreService(storetypes.NewKVStoreKey(feemarkettypes.StoreKey)).OpenKVStore(c)
+		if err := feemarketstore.Delete(skipKeyParams); err != nil {
+			return vm, err
+		}
+		if err := feemarketstore.Delete(skipKeyState); err != nil {
+			return vm, err
+		}
+		if err := feemarketstore.Delete(skipKeyEnabledHeight); err != nil {
+			return vm, err
+		}
+
+		// initialize new cosmos evm feemarket
+		keepers.FeeMarketKeeper.SetParams(ctx, feemarkettypes.DefaultParams())
 
 		vm, err := mm.RunMigrations(ctx, configurator, vm)
 		if err != nil {
