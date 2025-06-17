8cc56e88b7eadcf8990075bcd274b59b074b5085
freeelancer <lance.lan@mantra.finance>
2025-06-17 14:58:34 +0800

    allow only single evm msg

--- app/ante/evm.go
+++ app/ante/evm.go
@@ -2,12 +2,11 @@ package ante
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	evmante "github.com/cosmos/evm/ante/evm"
 )
 
 func newMonoEVMAnteHandler(options EVMHandlerOptions) sdk.AnteHandler {
 	return sdk.ChainAnteDecorators(
-		evmante.NewEVMMonoDecorator(
+		NewEVMMonoDecorator(
 			options.AccountKeeper,
 			options.FeeMarketKeeper,
 			options.EvmKeeper,
