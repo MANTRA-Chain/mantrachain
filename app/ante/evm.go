9e7976c2fb9421a6b8363e69ab160610afd07375
freeelancer <lance.lan@mantra.finance>
2025-06-17 15:15:13 +0800

    move msg length check to new antehandler

--- app/ante/evm.go
+++ app/ante/evm.go
@@ -2,11 +2,13 @@ package ante
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	evmante "github.com/cosmos/evm/ante/evm"
 )
 
-func newMonoEVMAnteHandler(options EVMHandlerOptions) sdk.AnteHandler {
+func newEVMAnteHandler(options EVMHandlerOptions) sdk.AnteHandler {
 	return sdk.ChainAnteDecorators(
-		NewEVMMonoDecorator(
+		EVMMsgCheckDecorator{},
+		evmante.NewEVMMonoDecorator(
 			options.AccountKeeper,
 			options.FeeMarketKeeper,
 			options.EvmKeeper,
