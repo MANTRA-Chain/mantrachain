a31932d73779728463b2e124e93ea721eeac1d5c
mmsqe <tqd0800210105@gmail.com>
2025-08-07 16:51:04 +0800

    add rc4

---
+++ app/upgrades/v5rc4/constants.go
@@ -0,0 +1,20 @@
+package v5rc4
+
+import (
+	"cosmossdk.io/store/types"
+	"github.com/MANTRA-Chain/mantrachain/v5/app/upgrades"
+)
+
+const (
+	// UpgradeName defines the on-chain upgrade name.
+	UpgradeName = "v5.0.0-rc4"
+)
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: types.StoreUpgrades{
+		Added:   []string{},
+		Deleted: []string{},
+	},
+}
