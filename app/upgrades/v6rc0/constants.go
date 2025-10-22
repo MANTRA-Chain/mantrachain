a9ca3abf37281bdfe4a3b3744090756b64924364
Leo Pang <34628052+allthatjazzleo@users.noreply.github.com>
2025-10-22 15:01:27 +0800

    fix: adding back v6rc0 (#496)
    Co-authored-by: yihuang <yi.huang@mantrachain.io>

---
+++ app/upgrades/v6rc0/constants.go
@@ -0,0 +1,20 @@
+package v6rc0
+
+import (
+	"cosmossdk.io/store/types"
+	"github.com/MANTRA-Chain/mantrachain/v6/app/upgrades"
+)
+
+const (
+	// UpgradeName defines the on-chain upgrade name.
+	UpgradeName = "v6.0.0-rc0"
+)
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: types.StoreUpgrades{
+		Added:   []string{},
+		Deleted: []string{"group"},
+	},
+}
