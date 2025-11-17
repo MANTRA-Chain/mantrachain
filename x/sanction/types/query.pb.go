dd2e49693686ae12c4ed83bf4a4bfbdf5dcb393f
freeelancer <lance.lan@mantra.finance>
2025-11-17 17:31:33 +0800

    remove all api proto gen

--- x/sanction/types/query.pb.go
+++ x/sanction/types/query.pb.go
@@ -362,6 +362,7 @@ func _Query_Blacklist_Handler(srv interface{}, ctx context.Context, dec func(int
 	return interceptor(ctx, in, info, handler)
 }
 
+var Query_serviceDesc = _Query_serviceDesc
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "mantrachain.sanction.v1.Query",
 	HandlerType: (*QueryServer)(nil),
