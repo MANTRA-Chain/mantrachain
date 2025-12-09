75c886dd9fe8e0651fbb2eb5808e78af98b9304c
freeelancer <lance.lan@mantra.finance>
2025-12-09 15:05:22 +0800

    corrected data

--- app/upgrades/v7rc3/before_upgrade_data.go
+++ app/upgrades/v7rc3/before_upgrade_data.go
@@ -3,12 +3,12 @@ package v7rc3
 var DryrunBeforeUpgrade = []byte(`
   {
     "mantravaloper143a99rce5u0p5l2tzy62hqjdl2dx6pmgxq4w78": {
-      "cumulative_reward_ratio": "0.096894012597024924",
-      "period": "999"
+      "cumulative_reward_ratio": "0.098805762397545508",
+      "period": "8005"
     },
     "mantravaloper16y6dq8ednxq2l7rtyws8n4rkfvkz4e6zu8t0uc": {
-      "cumulative_reward_ratio": "0.096894012597051663",
-      "period": "999"
+      "cumulative_reward_ratio": "0.098805762397572237",
+      "period": "8004"
     }
   }
 `)
@@ -47,12 +47,12 @@ var CanaryBeforeUpgrade = []byte(`
 var DukongBeforeUpgrade = []byte(`
   {
     "mantravaloper1q80heee98nyt47mlz69nyjj8ymm2r5858lnzrj": {
-      "cumulative_reward_ratio": "0.050956613406316154",
-      "period": "9992"
+      "cumulative_reward_ratio": "1.295031868786635602",
+      "period": "84263"
     },
     "mantravaloper1q8mgs55hfgkm7d5rret439997x87s2ek2r83q2": {
-      "cumulative_reward_ratio": "351.119181496183189227",
-      "period": "99999"
+      "cumulative_reward_ratio": "361.095312936214945461",
+      "period": "170886"
     },
     "mantravaloper1pxw9pjusq42fuzy3w54txzy4vcpxq2kjnyf5ax": {
       "period": "1"
@@ -62,137 +62,143 @@ var DukongBeforeUpgrade = []byte(`
       "period": "47"
     },
     "mantravaloper1ptjpppz48vyty6jw5cpegch3xq67c55utpd045": {
-      "period": "960"
+      "cumulative_reward_ratio": "0.012395741337729195",
+      "period": "4195"
     },
     "mantravaloper1p3sl4ttvxq8er0x2vnsgfpgp704tsyspnxsf8f": {
       "cumulative_reward_ratio": "0.946833444715882215",
       "period": "58"
     },
     "mantravaloper1r5fjdrv6exgx43edyjx0k7m4yayuemdwljd0ak": {
-      "cumulative_reward_ratio": "0.108829412713078103",
-      "period": "9"
+      "cumulative_reward_ratio": "2.494646798265962464",
+      "period": "51"
     },
     "mantravaloper1r6yj27sh3c982dh34qkw8yvrw2l5f0z9ypaf52": {
-      "cumulative_reward_ratio": "0.432907053769915786",
-      "period": "9"
+      "cumulative_reward_ratio": "8.624789130156581099",
+      "period": "638"
     },
     "mantravaloper1rlawn3c8wtjy5axahkg23le005607s0ap6t8xe": {
       "period": "2"
     },
     "mantravaloper1yhwm0gtusg758w32h3eszfnrhnmznvm8k2fhu2": {
-      "cumulative_reward_ratio": "0.566445905487524634",
-      "period": "86"
+      "cumulative_reward_ratio": "0.786224452190254714",
+      "period": "128"
     },
     "mantravaloper19ytqqp6eq56vjzpzz78gccsxk3nfcxahavqp7k": {
-      "cumulative_reward_ratio": "0.200650664592401612",
-      "period": "9"
+      "cumulative_reward_ratio": "2.244795701370063448",
+      "period": "153"
     },
     "mantravaloper1xy7s2564rgk8e99xgagl0md6ckxgt0k32lkvkc": {
-      "cumulative_reward_ratio": "2.661078860682830417",
-      "period": "97"
+      "cumulative_reward_ratio": "2.661080547741611527",
+      "period": "102"
     },
     "mantravaloper18jywgwczf0qt4q40tnr9rmzyjmccawyknyswhz": {
-      "cumulative_reward_ratio": "351.119133863931985307",
-      "period": "99992"
+      "cumulative_reward_ratio": "361.092920015930577171",
+      "period": "173615"
     },
     "mantravaloper184pt4r8e27f8ndtspj8fs4asxw8y3gqcftw5v5": {
-      "cumulative_reward_ratio": "0.194827961372726547",
-      "period": "8"
+      "cumulative_reward_ratio": "1.372484659552788487",
+      "period": "42"
     },
     "mantravaloper18hwe5xgkdqmlwk5a4vmyfge4zlx0x8ntexk06t": {
-      "period": "948"
+      "cumulative_reward_ratio": "6.008763030940801582",
+      "period": "41177"
     },
     "mantravaloper12pg93tjl0hsjctk4h2u8lj8m7z6dzp8aa0ss5m": {
-      "cumulative_reward_ratio": "0.221547678854987769",
-      "period": "9"
+      "cumulative_reward_ratio": "0.568368604445873578",
+      "period": "11"
     },
     "mantravaloper12z6m4uh5grlsynhgyevnv4pc3qma0q8f3cjru2": {
-      "cumulative_reward_ratio": "0.052259214638236097",
-      "period": "9998"
+      "cumulative_reward_ratio": "8.256830233105136189",
+      "period": "77005"
     },
     "mantravaloper12ywfr0g0ukl838c7ek3fl522auymwuctt243sc": {
       "cumulative_reward_ratio": "0.750454682886486215",
       "period": "59"
     },
     "mantravaloper1tqfutxjqf5tqw5uq5lgxx7thrjqey95l2pf9f3": {
-      "period": "914"
+      "cumulative_reward_ratio": "0.462939621439201210",
+      "period": "35230"
     },
     "mantravaloper1t3g4vylrgun8k4wm5dlw8hmcn5x0p6jvjgk6c2": {
-      "cumulative_reward_ratio": "351.086088082202955953",
-      "period": "99993"
+      "cumulative_reward_ratio": "362.173074119019237092",
+      "period": "232485"
     },
     "mantravaloper1vyrsyxx9flxegw6y4m8d97pkp3pmy9mnawvk3h": {
-      "cumulative_reward_ratio": "2.059931929533144046",
-      "period": "92"
+      "cumulative_reward_ratio": "6.654218733424759882",
+      "period": "448"
     },
     "mantravaloper1drm8x4j2yvrjg9jcqnk0kha7vr0f2j0gcau3vg": {
-      "cumulative_reward_ratio": "0.118598088371349942",
-      "period": "99983"
+      "cumulative_reward_ratio": "9.760223223095560295",
+      "period": "141269"
     },
     "mantravaloper1dfad2l9hxxn7tasu73yvldx6v8l9evkztlgnv6": {
-      "cumulative_reward_ratio": "0.012624837665408735",
-      "period": "9"
+      "cumulative_reward_ratio": "0.338932490263634407",
+      "period": "15"
     },
     "mantravaloper1w9np7x84tenkhhxcvz290jw8tefe57qef8wkw3": {
-      "cumulative_reward_ratio": "351.127144691560713237",
-      "period": "99983"
+      "cumulative_reward_ratio": "361.092931294809300968",
+      "period": "125084"
     },
     "mantravaloper1ws65ht8j258spyat6d9ughny38dcrd2zshfq3g": {
-      "period": "981"
+      "cumulative_reward_ratio": "3.104125838453894718",
+      "period": "38900"
     },
     "mantravaloper102nulk7g4ttg9ynmkhjdndtrrlwd7hjuaf0j2d": {
-      "period": "8596"
+      "cumulative_reward_ratio": "1.409259949795797059",
+      "period": "27071"
     },
     "mantravaloper1svyfspn2nnedgy47e09xg2e5fh0k224qk8zgha": {
-      "cumulative_reward_ratio": "0.274897066723866587",
-      "period": "42"
+      "cumulative_reward_ratio": "3.005828763893313065",
+      "period": "313"
     },
     "mantravaloper1s473qnfcamyys0jkkjdmtddyunas73sjsngrwp": {
       "period": "1"
     },
     "mantravaloper1safytq0lmge66azvp064mcxusfk5exu3ymw772": {
-      "cumulative_reward_ratio": "0.254293263629651250",
-      "period": "9"
+      "cumulative_reward_ratio": "2.173918893891591164",
+      "period": "48"
     },
     "mantravaloper13zktdh2vcjmwa8hs8syqvr90hzlrv6uz9vecee": {
-      "cumulative_reward_ratio": "350.918745894424078713",
-      "period": "9"
+      "cumulative_reward_ratio": "350.983456873619718610",
+      "period": "54"
     },
     "mantravaloper13t42tpwk25mdwm7wj7k5z0r9vj4jt0dk9gunyx": {
       "cumulative_reward_ratio": "2.308224379328916784",
       "period": "71"
     },
     "mantravaloper13nwzm5dfd26ue74jr6sc39gyn3qze0rjg5mgk4": {
-      "period": "9994"
+      "period": "10541"
     },
     "mantravaloper134vcayn3jmv0sa0e26qxdhekcnvzvf6s7m8pav": {
-      "cumulative_reward_ratio": "351.118818658467665476",
-      "period": "99999"
+      "cumulative_reward_ratio": "361.092925788908843222",
+      "period": "167698"
     },
     "mantravaloper136ydt55a2kuumkv93xqhz00jel53p4sq9zl7qw": {
-      "period": "85"
+      "cumulative_reward_ratio": "0.004373575773227900",
+      "period": "38928"
     },
     "mantravaloper136m3l5erz9w936phstxt0fwg02q44rztpx3ny4": {
-      "cumulative_reward_ratio": "0.016298261129997287",
-      "period": "997"
+      "cumulative_reward_ratio": "3.312025593990562254",
+      "period": "59877"
     },
     "mantravaloper15vpa2hxed7gak5gsu2cksx4uwetlt99x2zfxg3": {
       "period": "1"
     },
     "mantravaloper150nxp8ukd42pjnsqagvj59a6uz9smzugjjczfh": {
-      "cumulative_reward_ratio": "0.470046636796392844",
-      "period": "9"
+      "cumulative_reward_ratio": "0.528388653237713688",
+      "period": "11"
     },
     "mantravaloper1kyql9w2m9s3c5jv8upgw6umkltpfu82lqylhj5": {
       "period": "1"
     },
     "mantravaloper1k8hnt5vd4ml37huzvcg32jsa86el6fhytzkg7y": {
-      "cumulative_reward_ratio": "0.507518730549608905",
-      "period": "53"
+      "cumulative_reward_ratio": "3.174334126533363266",
+      "period": "172"
     },
     "mantravaloper1kj4cghlh797cdwf3ca7n3fr0p8e0mpyyk0f3tj": {
-      "cumulative_reward_ratio": "0.245162124406858934",
-      "period": "9981"
+      "cumulative_reward_ratio": "10.078944668747053088",
+      "period": "93451"
     },
     "mantravaloper1hf65ux42ju3k2esjndyeh67c74d9v0zqrltsh6": {
       "cumulative_reward_ratio": "4.120666026361879084",
@@ -203,8 +209,8 @@ var DukongBeforeUpgrade = []byte(`
       "period": "3"
     },
     "mantravaloper1hsq6yc7fzqfuwkmawjw8jzkgy9zmrlhmp7gsf3": {
-      "cumulative_reward_ratio": "0.030545829619190405",
-      "period": "9805"
+      "cumulative_reward_ratio": "0.669252183401386886",
+      "period": "78790"
     },
     "mantravaloper1cpmeqngzn6w6dscew80kr0wwjy6skr2u0jmgwe": {
       "period": "1"
@@ -214,34 +220,35 @@ var DukongBeforeUpgrade = []byte(`
       "period": "3"
     },
     "mantravaloper1c22m5xwqmxfw498ru47kj5fcmwxz7uzdajyydq": {
-      "period": "9"
+      "period": "30"
     },
     "mantravaloper1c0fs6tz3t50jt6ukdwnz6w658plrnv6ux64nrk": {
-      "cumulative_reward_ratio": "0.006154191470994450",
-      "period": "9947"
+      "cumulative_reward_ratio": "10.348922547693135134",
+      "period": "73178"
     },
     "mantravaloper1carsvyd6280z7gju0cmxs3tcpg9edp5e93aq0l": {
-      "cumulative_reward_ratio": "351.130245490119979808",
-      "period": "99995"
+      "cumulative_reward_ratio": "361.073136139643279436",
+      "period": "105486"
     },
     "mantravaloper1ea4hlqfskjvn0ldenw8gv7jjdzrljcchl7deme": {
-      "cumulative_reward_ratio": "0.069378001133254561",
-      "period": "9990"
+      "cumulative_reward_ratio": "9.107222794390698850",
+      "period": "193844"
     },
     "mantravaloper16vderh4096zsh9e9ccpypuz86mphkc5pc9cyur": {
-      "cumulative_reward_ratio": "0.020124064605206819",
-      "period": "8"
+      "cumulative_reward_ratio": "1.176961397966315265",
+      "period": "46"
     },
     "mantravaloper1mrjcnapkcga8vflum88r33r0hedg36uzh0shun": {
-      "period": "8812"
+      "cumulative_reward_ratio": "3.123305542869369238",
+      "period": "38073"
     },
     "mantravaloper1myqld5l5fm6u2v48866xdcy3h6nw86e8ggqrw2": {
       "cumulative_reward_ratio": "0.004269325777613962",
       "period": "3"
     },
     "mantravaloper1urt9p8p0dshgumjaptcr8e64eq6ce53vsh6h45": {
-      "cumulative_reward_ratio": "0.172706990471514708",
-      "period": "8"
+      "cumulative_reward_ratio": "0.182031471852799045",
+      "period": "23"
     },
     "mantravaloper1u5lrjadekj45tp2c8zszcazl5dnkq7skuwyk8d": {
       "cumulative_reward_ratio": "0.012375787770553461",
@@ -252,14 +259,12 @@ var DukongBeforeUpgrade = []byte(`
       "period": "49"
     },
     "mantravaloper1aysxyetv0y7sfe0v70akyjn3kkjc6qyt5dlewa": {
-      "cumulative_reward_ratio": "1.819742377080987301",
-      "period": "98"
+      "cumulative_reward_ratio": "2.689341379798239250",
+      "period": "102"
     },
     "mantravaloper1l3zxwfypzfp2dxcwaqnq8rx8rwzq3cl7js39sv": {
-      "period": "9988"
-    },
-    "mantravaloper1l5le3ef4gx0y23amx693m2ku086zkx43txg7z0": {
-      "period": "2"
+      "cumulative_reward_ratio": "1.257844020387063205",
+      "period": "13857"
     },
     "mantravaloper1l7tundpmjgh022rufg2l2pr8pxj7c9k5kl2s7e": {
       "cumulative_reward_ratio": "0.505949679143216324",
