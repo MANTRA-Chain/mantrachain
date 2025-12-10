3867f8fca1780c3d8ae6ee40fe571743ca530939
freeelancer <lance.lan@mantra.finance>
2025-12-10 16:12:25 +0800

    add v7rc4

---
+++ app/upgrades/v7rc4/constants.go
@@ -0,0 +1,20 @@
+package v7rc4
+
+import (
+	"cosmossdk.io/store/types"
+	"github.com/MANTRA-Chain/mantrachain/v7/app/upgrades"
+)
+
+const (
+	// UpgradeName defines the on-chain upgrade name.
+	UpgradeName = "v7.0.0-rc4"
+)
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: types.StoreUpgrades{
+		Added:   []string{},
+		Deleted: []string{},
+	},
+}
