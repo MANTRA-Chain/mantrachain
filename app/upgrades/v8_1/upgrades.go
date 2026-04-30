2f8f02ff8606e6e3a2089baa4a320fd83324c42b
mmsqe <tqd0800210105@gmail.com>
2026-04-30 16:25:38 +0800

    repairSilentSlashes
    fix plan

--- app/upgrades/v8_1/upgrades.go
+++ app/upgrades/v8_1/upgrades.go
@@ -21,13 +21,22 @@ func CreateUpgradeHandler(
 		ctx.Logger().Info("Starting v8.1.0 upgrade...")
 
 		// Repair before RunMigrations so reward-touching migrations don't panic.
-		ctx.Logger().Info("Repairing distribution state for silently-skipped slashes...")
+		ctx.Logger().Info("Clamping silently-skipped slash residue on delegator starting info...")
 		if err := fixSilentlySkippedSlashes(ctx, keepers.StakingKeeper, keepers.DistrKeeper); err != nil {
 			return vm, err
 		}
 
+		ctx.Logger().Info("Resetting starting info on silent-slash validators...")
+		records, err := resolveSilentSlashes(plan)
+		if err != nil {
+			return vm, err
+		}
+		if err := repairSilentSlashes(ctx, keepers.StakingKeeper, keepers.DistrKeeper, records); err != nil {
+			return vm, err
+		}
+
 		ctx.Logger().Info("Running module migrations...")
-		vm, err := mm.RunMigrations(ctx, configurator, vm)
+		vm, err = mm.RunMigrations(ctx, configurator, vm)
 		if err != nil {
 			return vm, err
 		}
