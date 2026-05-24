b29a7a3cdc26fd2c833059991a0d36f78126819e
freeelancer <lance.lan@mantra.finance>
2026-05-24 10:56:25 +0800

    feat: Increase max mca tax

--- x/tax/types/params.go
+++ x/tax/types/params.go
@@ -10,7 +10,7 @@ import (
 
 // Declare defaults for MCA tax and MCA address
 var (
-	DefaultMcaTax     = "0.4"
+	DefaultMcaTax     = "0.6"
 	DefaultMcaAddress = "mantra15m77x4pe6w9vtpuqm22qxu0ds7vn4ehzwx8pls"
 	MaxMcaTax         = math.LegacyMustNewDecFromStr("0.4") // 40 %
 )
